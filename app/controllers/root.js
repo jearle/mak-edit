2cf8415ee377c5eb460edec2f3e704efbfd31fe6
Jesse Earle <jesse.earle@high5games.com>
2014-04-09 08:22:08 -0400

    removed tabs

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -4,35 +4,35 @@ var hljs = require('highlight.js');
 
 app.controller('Root', [
 
-	'$scope',
-	'jeThemes',
-	'jeCss',
-	'jeMarkdown',
-	'jeLocalStorage',
-	'jeAceEditorScriptNode',
+  '$scope',
+  'jeThemes',
+  'jeCss',
+  'jeMarkdown',
+  'jeLocalStorage',
+  'jeAceEditorScriptNode',
 
-	function ($scope, Themes, Css, Markdown, LocalStorage, scriptNode) {
+  function ($scope, Themes, Css, Markdown, LocalStorage, scriptNode) {
 
-		Css.addStyle(Themes.tomorrow(), 'je_tomorrow_theme');
+    Css.addStyle(Themes.tomorrow(), 'je_tomorrow_theme');
 
-		$scope.markdown = LocalStorage.storage();
+    $scope.markdown = LocalStorage.storage();
 
-		function markdownChanged (newValue, oldValue) {
+    function markdownChanged (newValue, oldValue) {
 
-			if (newValue === oldValue) return;
+      if (newValue === oldValue) return;
 
-			scriptNode.trigger(
-				'markdownChanged',
-				{
-					markdown: $scope.markdown
-				}
-			);
+      scriptNode.trigger(
+        'markdownChanged',
+        {
+          markdown: $scope.markdown
+        }
+      );
 
-			LocalStorage.storage($scope.markdown);
-		}
+      LocalStorage.storage($scope.markdown);
+    }
 
-		$scope.$watch('markdown', markdownChanged);
+    $scope.$watch('markdown', markdownChanged);
 
-	}
+  }
 
 ]);
